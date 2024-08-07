def4715736bf66951b6fca26101a1c477729595e
Meandr58 <andrey.mescherin@gmail.com>
2024-08-07 11:49:29 +0300

    base-corrected

--- divanpars/divanpars/spiders/divannewpars.py
+++ divanpars/divanpars/spiders/divannewpars.py
@@ -7,11 +7,10 @@ class DivannewparsSpider(scrapy.Spider):
     start_urls = ["https://divan.ru/category/divany-i-kresla"]
 
     def parse(self, response):
-        def parse(self, response):
-            divans = response.css('div._Ud0k')
-            for divan in divans:
-                yield {
-                    'name': divan.css('div.lsooF span::text').get(),
-                    'price': divan.css('div.pY3d2 span::text').get(),
-                    'url': divan.css('a').attrib['href']
-                }
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                'name': divan.css('div.lsooF span::text').get(),
+                'price': divan.css('div.pY3d2 span::text').get(),
+                'url': divan.css('a').attrib['href']
+            }
